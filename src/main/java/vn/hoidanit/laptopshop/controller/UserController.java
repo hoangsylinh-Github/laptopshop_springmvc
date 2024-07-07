bc24e42bcd665480981de6eddf228516ed1560d5
Hoang Linh <hoangsylinh31012003@gmail.com>
2024-07-07 18:34:54 +0700

    #53

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,11 +1,14 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // mo hinh MVC
 @Controller
@@ -17,9 +20,18 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
-        return "view.html";
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("hoanglinh", test);
+        model.addAttribute("linhchuot", "from controller with model");
+        return "index";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        return "admin/user/create";
+    }
+
 }
 
 // MO HINH restfulAPI
