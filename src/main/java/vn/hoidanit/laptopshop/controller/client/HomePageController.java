215ce2bf9b82623962ce5d4786e17ec5d8f90ac2
Hoang Linh <hoangsylinh31012003@gmail.com>
2024-10-18 17:43:36 +0700

    ux register adn login

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import vn.hoidanit.laptopshop.domain.Product;
 import vn.hoidanit.laptopshop.service.ProductService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomePageController {
@@ -24,4 +25,13 @@ public class HomePageController {
         return "client/homePage/show";
     }
 
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        return "client/auth/register";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "client/auth/login";
+    }
 }
